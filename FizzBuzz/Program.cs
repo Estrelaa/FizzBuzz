3603e111f988ec5f8e356f09e410f11eb45d6445
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-18 12:36:54 +0000

    Now allows the user to enter input for the max range

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,11 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 256; i++)
+            int MaxValue = 0;
+
+            Console.WriteLine("Enter the max range: ");
+            MaxValue = int.Parse(Console.ReadLine());
+            for (int i = 0; i < MaxValue; i++)
             {
                 // Makes sure its reset every time in the loop
                 string Result = "";
@@ -61,7 +65,16 @@ namespace FizzBuzz
                 {
                     Result = i.ToString();
                 }
+                /*
+                if (i % 17 == 0)
+                {
+                    for (int j = 0; j < Result.Length; j++)
+                    {
+                        
+                    }
+                }
                 // Print the result to the console
+                */
                 Console.WriteLine(Result);
             }
             // Just to keep the console open until a key is pressed
