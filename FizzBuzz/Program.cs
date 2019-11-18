5440fcca834985cadd07661e944f53ef06869ca9
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-18 11:33:05 +0000

    Prints numbers upto 100

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,10 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
         }
     }
