45050fcdb08508febb9586812f85582e5092fc17
Kieran Winfield <K.Winfield@ucas.ac.uk>
2019-11-18 16:59:23 +0000

    Begun changing 'Fezz' into its own function

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FizzBuzz
 {
@@ -7,6 +8,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            // Variables need for the program to work
             int MaxValue = 0;
             List<string> Result = new List<string>();
 
@@ -17,40 +19,28 @@ namespace FizzBuzz
             {
                 // Makes sure Result resets every time in the loop
                 Result.Clear();
-                
-                
-                if (i % 7 == 0)
-                {
-                    if (i % 13 == 0)
-                    {
-                        Result.Add("Fezz");
-                    }
-                    Result.Add("Bang");
-                }
+                               
+             
                 if (i % 3 == 0 && i % 5 ==0)
                 {
                     Result.Add("Fizz");
-                    if (i % 13 == 0)
-                    {
-                        Result.Add("Fezz");
-                    }
+                    Fezz(Result, i);
                     Result.Add("Buzz");
                 }
                  else if (i % 3 == 0)
                 {
                     Result.Add("Fizz");
-                    if (i % 13 == 0)
-                    {
-                        Result.Add("Fezz");
-                    }
+                    Fezz(Result, i);
                 }
                 else if (i % 5 == 0)
                 {
-                    if (i % 13 == 0)
-                    {
-                        Result.Add("Fezz");
-                    }
+                    Fezz(Result, i);
                     Result.Add("Buzz");
+                }
+                if (i % 7 == 0)
+                {
+                    Fezz(Result, i);
+                    Result.Add("Bang");
                 }
                 else if (i % 11 == 0)
                 {
@@ -64,21 +54,38 @@ namespace FizzBuzz
                         Result.Add("Bong");
                     }
                 }
+                if (i % 17 == 0)
+                {
+                    Result.Reverse();
+                }
                 else
                 {
                     Result.Add(i.ToString());
                 }
 
-                if (i % 17 == 0)
-                {
-                    Result.Reverse();
-                }
                 // Print the result to the console
                 string Combined = string.Join("", Result);
                 Console.WriteLine(Combined);
             }
             // Just to keep the console open until a key is pressed
             Console.ReadLine();
+        }
+
+        private static List<string> Fezz(List<string> Result, int i)
+        {
+
+            if (i % 13 == 0)
+            {
+                foreach (string word in Result)
+                {
+                    if (word == "Buzz" | word == "Bang")
+                    {
+
+                    }
+                }
+               
+            }
+            return Result;
         }
     }
 }
